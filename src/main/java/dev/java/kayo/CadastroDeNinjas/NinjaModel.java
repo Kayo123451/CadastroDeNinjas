8bfb5cfa6c2711eb60edc46e9c9c43ad4626d4a7
Kayo123451 <kayousouza11@gmail.com>
2026-06-15 19:56:59 -0300

    Cria uma entidade no banco de dados a partir do Ninja Model

---
+++ src/main/java/dev/java/kayo/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,50 @@
+package dev.java.kayo.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//Entity transforma a classe em uma entidade do banco de dados
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+//  Faz o java controlar pra mim com o ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
