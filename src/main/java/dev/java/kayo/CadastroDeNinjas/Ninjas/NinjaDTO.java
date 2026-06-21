9c5a5c3e4703b42e5654d42aafed9d36da608072
Kayo123451 <kayousouza11@gmail.com>
2026-06-21 16:50:43 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no ninjaMapper os caminhos entidade x DTO

--- src/main/java/dev/java/kayo/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java/kayo/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.java.kayo.CadastroDeNinjas.Ninjas;
 
+import dev.java.kayo.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
+
+
 }
--- src/main/java/dev/java/kayo/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java/kayo/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,38 @@
 package dev.java.kayo.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+
+    }
+        public NinjaDTO map(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+            ninjaDTO.setNome(ninjaModel.getNome());
+            ninjaDTO.setIdade(ninjaModel.getIdade());
+            ninjaDTO.setEmail(ninjaModel.getEmail());
+            ninjaDTO.setId(ninjaModel.getId());
+            ninjaDTO.setRank(ninjaModel.getRank());
+            ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+            return ninjaDTO;
+
+        }
+
+
+
 }
